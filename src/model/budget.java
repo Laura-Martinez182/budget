9567ef9a7e324be6f02d3fe434eff2917b87a2dd
Laura Martinez <laura.martinez4@correo.icesi.edu.co>
2020-09-16 08:32:01 -0500

    metodos de separacion por utilidad

--- src/model/budget.java
+++ src/model/budget.java
@@ -7,59 +7,62 @@ public class budget {
 	private final static double FINAL_WORK = 2600000;
 	private final static double PAINT = 980000;
 	
-	private final static string obraNegra = "";
-	private final static string obraBlanca = "";
-	private final static string pintura = "";
 	
-	
-		public static double total_store(homecenter_prices, centerstore_prices, neighborhood_prices, work) {
-			Scanner ab = new Scanner(System.in);
-			
-			double delivery = 0;
-			double[] total_price =	main.price_for_each_place(ab, result);
-			double total_price = 0;
+		public static double total_store(int[] materials_price, int[] materials_quantity, int general_amount, String location) {
+			int total_price = 0;
 			for (int i = 0; i < result; i++) {
-			total_price += (each_amount[i]*total_price[i]);
-		}
-		return total_price;		
-}	
-
-	if(name){
-            if(name.equals("homecenter")){
-                if(total_price<80000){
-                    delivery=120000;
-                }
-                if(total_price<300000){
-                    delivery=28000;
-                }
-                if(total_price>=300000){
-                    delivery=120000;
-                }
-            if(name.equals("ferreteria del centro")){
-                if(total_price<80000){
-                    delivery=50000;
-                }
-                if(total_price<300000){
-                    delivery=0;
-                }
-                if(total_price>=300000){
-                    delivery=0;
-                }
-            if(name.equals("ferreteria del barrio")){
-                if(total_price<80000){
-                    delivery=120000;
-                }
-                if(total_price<300000){
-                    delivery=55000;
-                }
-                if(total_price>=300000){
-                    delivery=0;
-                }
+				total_price += (materials_quantity[i]* materials_price[i]);
+			}	
+				if (total_price < 80000) {
+					if (location.equalsIgnoreCase("Norte") || location .equalsIgnoreCase("Sur")) {
+						total_price	+= 120000;
+				}else if (location.equalsIgnoreCase("Centro")) {
+					total_price += 50000;
+				}
 			}
-          }
-        }
-		return delivery;
-      }
+			else if (total_price < 300000) {
+				if (location.equalsIgnoreCase("Norte")) {
+					total_price += 28000;
+			}else if(location.equalsIgnoreCase("Sur")) {
+					total_price += 55000;
+			}
+		
+		}
+			total_price += (ROUGH_CONSTRUCTION + FINAL_WORK + PAINT);
+				return total_price;
+	}
+
+
+
+		public static int utilizationGroups (String[] work, String typeUtilization) {
+			int count = 0; //Contador de materiales en el mismo grupo
+				for (int a = 0; a < work.length(); a++) {
+					if (work[a].equalsIgnoreCase(typeUtilization)) {
+							count++;
+			}
+			return count;
+	}
+	
+	
+	
+		public static String[] utilizationsArrays (String[] work, String typeUtilization, String name) {
+			int groups = utilizationGroups(work);
+			String[] separation = new String [groups];
+			int position = 0; // variable entera que indica la posicion en el arreglo donde esta el material de esa utilidad
+				for (int a = 0; a < work.length(); a++) {
+					if (work[a].equalsIgnoreCase(typeUtilization)){
+						separation[position] = work[a];
+							position++;
+						}
+					}
+			
+				return separation;
+	}
+
+				
+				
+
+	
      
 	
 }
--- src/ui/main.java
+++ src/ui/main.java
@@ -3,51 +3,61 @@ import model.*;
 import java.util.Scanner;
 public class main {
 	
-	public final static String NORTE = "";
-	public final static String CENTRO = "";
-	public final static String SUR = "";
 	
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		int result = ask_for_general_amount(sc);
 		String[] name = ask_for_product_name(sc, result);
-		double[] each_amount = ask_for_product_amount(sc, result);
+		int[] each_amount = ask_for_product_amount(sc, result);
 		String[] work = utilization (sc, result);
-		double[] homecenter_prices = price_for_each_place (sc, result, "homecenter");
-		double[] centerstore_prices = price_for_each_place (sc, result, "ferreteria del centro");
-		double[] neighborhood_prices = price_for_each_place (sc, result, "ferreteria del barrio");
+		String location = delivery (sc);
+		
+		int[] homecenter_prices = price_for_each_place (sc, result, "homecenter");
+		int[] centerstore_prices = price_for_each_place (sc, result, "ferreteria del centro");
+		int[] neighborhood_prices = price_for_each_place (sc, result, "ferreteria del barrio");
+		
+		System.out.println ("El precio total para homecenter es: " + budget.total_store(homecenter_prices, each_amount, result, location));
+		System.out.println ("El precio total para la ferreteria del centro es: " + budget.total_store(centerstore_prices, each_amount, result, location));
+		System.out.println ("El precio total para la ferreteria del barrio es: " + budget.total_store(neighborhood_prices, each_amount, result, location));
+		
+		System.out.println ("Productos para obra negra: " + budget.utilizationsArrays(work, "obra negra", name);
+		System.out.println ("Productos para obra blanca: " + budget.utilizationsArrays(work, "obra blanca", name);
+		System.out.println ("Productos para pintura: " + budget.utilizationsArrays(work, "pintura", name);
+		
+		
 	}
 
 		public static int ask_for_general_amount(Scanner sc) {
-		System.out.println ("Ingrese el numero de materiales necesarios para llevar a cabo la obra:");
-		int num_products = sc.nextInt();
-		sc.nextLine();
-		return num_products;
-	}
+			System.out.println ("Ingrese el numero de materiales necesarios para llevar a cabo la obra:");
+			int num_products = sc.nextInt();
+			sc.nextLine();
+			return num_products;
+		}
 		
 		public static String[] ask_for_product_name (Scanner sc, int general_amount) {
-		String[] product_Name = new String [general_amount];
-		for (int i = 0; i < general_amount; i++) {
+			String[] product_Name = new String [general_amount];
+			for (int i = 0; i < general_amount; i++) {
 			System.out.println ("Ingrese el nombre del material " + (i+1) + ":");
 			product_Name[i] = sc.nextLine().toUpperCase();
 			sc.nextLine();
+			}
+				return product_Name;
 		}
-			return product_Name;
-	}
 	
-		public static double[] ask_for_product_amount (Scanner sc, int general_amount) {
-		double[] each_product_amount = new double [general_amount];
-		for (int i = 0; i < general_amount; i++) {
-		System.out.println ("Ingrese la cantidad necesitada del material " + (i+1) + ":");	
-		each_product_amount[i] = sc.nextDouble();
-		sc.nextLine();
+		public static int[] ask_for_product_amount (Scanner sc, int general_amount) {
+			int[] each_product_amount = new int [general_amount];
+			for (int i = 0; i < general_amount; i++) {
+			System.out.println ("Ingrese la cantidad necesitada del material " + (i+1) + ":");	
+			each_product_amount[i] = sc.nextInt();
+			sc.nextLine();
+			}
+				return each_product_amount;
 		}
-			return each_product_amount;
-	}
 	
 		public static String[] utilization (Scanner sc, int general_amount) {
 			String[] each_product_type = new String [general_amount];
-			System.out.println ("Ingrese una de las siguientes opciones de obra: obraNegra || obraBlanca || pintura");		
+			System.out.println ("Ingrese una de las siguientes opciones de obra: obra negra || obra blanca || pintura");		
 			for (int i = 0; i < general_amount; i++) {
 			System.out.println ("Ingrese la utilidad del material " + (i+1) + ":");		
 			each_product_type[i] = sc.nextLine().toUpperCase();
@@ -56,12 +66,12 @@ public class main {
 			  return each_product_type;
 		}
 			
-		public static double[] price_for_each_place (Scanner sc, int general_amount, String name) {
-			double[] price = new double[general_amount];
+		public static int[] price_for_each_place (Scanner sc, int general_amount, String name) {
+			int[] price = new int[general_amount];
 			System.out.println ("Ingrese el nombre del estableciemiento" + name); 
 			for (int i = 0; i < general_amount; i++) {
 			System.out.println ("Ingrese el precio del material " + (i+1));		
-			price[i] = sc.nextDouble();
+			price[i] = sc.nextInt();
 			sc.nextLine();
 				
 			}
@@ -75,6 +85,9 @@ public class main {
 			sc.nextLine();
 			return location;
 		}
+		
+		
+	
 			
 }
 		
